80c4d2bbbb3faae64af7b2e56bd1a45e3e954442
Vitor Correa <vitor.correa@localpayment.com>
2022-12-14 12:08:31 +0100

    Finish docker

--- nodes.py
+++ nodes.py
@@ -1,56 +1,48 @@
 import csv
 import requests
-import sys
 import os
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 
-simulator_url = 'http://localhost:' + os.environ['SIMULATOR_PORT']
+simulator_url = 'http://simulator:' + os.environ['SIMULATOR_PORT']
 
-def simulate_data_streaming(row_limit):
-    n = 0
-    with open('input.csv') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        headers = next(reader)
-        for row in reader:
-            lat_a, lat_b, lat_c, cpu_a, ram_a, cpu_b, ram_b, cpu_c, ram_c, x_c, y_c = row
-            payload = [
-                {
-                    "id": 0,
-                    "lat": lat_a,
-                    "cpu": cpu_a,
-                    "ram": ram_a,
-                    "x": 0,
-                    "y": 0,
-                },
-                {
-                    "id": 1,
-                    "lat": lat_b,
-                    "cpu": cpu_b,
-                    "ram": ram_b,
-                    "x": 1,
-                    "y": 2,
-                },
-                {
-                    "id": 2,
-                    "lat": lat_c,
-                    "cpu": cpu_c,
-                    "ram": ram_c,
-                    "x": x_c,
-                    "y": y_c,
-                },
-            ]
-            requests.post(simulator_url + '/new', json=payload)
-            print(f"sent row {payload}")
+time.sleep(5)
 
-            n += 1
-            if n >= row_limit:
-                break
+n = 0
+with open('input.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    headers = next(reader)
+    for row in reader:
+        time.sleep(int(os.environ['NODES_INTERVAL']))
 
-
-if len(sys.argv) != 2:
-    print("Pass number of rows to simulate as parameter")
-    exit(-1)
-
-simulate_data_streaming(int(sys.argv[1]))
+        lat_a, lat_b, lat_c, cpu_a, ram_a, cpu_b, ram_b, cpu_c, ram_c, x_c, y_c = row
+        payload = [
+            {
+                "id": 0,
+                "lat": lat_a,
+                "cpu": cpu_a,
+                "ram": ram_a,
+                "x": 0,
+                "y": 0,
+            },
+            {
+                "id": 1,
+                "lat": lat_b,
+                "cpu": cpu_b,
+                "ram": ram_b,
+                "x": 1,
+                "y": 2,
+            },
+            {
+                "id": 2,
+                "lat": lat_c,
+                "cpu": cpu_c,
+                "ram": ram_c,
+                "x": x_c,
+                "y": y_c,
+            },
+        ]
+        requests.post(simulator_url + '/new', json=payload)
+        print(f"sent row {payload}")
--- scorer.py
+++ scorer.py
@@ -1,22 +1,26 @@
 import os
 import requests
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
 
-simulator_url = 'http://localhost:' + os.environ['SIMULATOR_PORT']
+simulator_url = 'http://simulator:' + os.environ['SIMULATOR_PORT']
 
-nodes = requests.get(simulator_url).json()
 
-payload = {}
+while True:
+    time.sleep(int(os.environ['SCORER_INTERVAL']))
 
-for node in nodes:
-    payload[node] = {}
-    cpu_history = [float(cpu) for cpu in nodes[node]["cpu"]]
-    ram_history = [float(ram) for ram in nodes[node]["ram"]]
-    cpu_score = round(sum(cpu_history) / len(cpu_history), 2)
-    ram_score = round(sum(ram_history) / len(ram_history), 2)
-    payload[node]["cpu_score"] = cpu_score
-    payload[node]["ram_score"] = ram_score
+    payload = {}
 
-requests.post(simulator_url + '/update_scores', json=payload)
+    nodes = requests.get(simulator_url).json()
+    for node in nodes:
+        payload[node] = {}
+        cpu_history = [float(cpu) for cpu in nodes[node]["cpu"]]
+        ram_history = [float(ram) for ram in nodes[node]["ram"]]
+        cpu_score = round(sum(cpu_history) / len(cpu_history), 2)
+        ram_score = round(sum(ram_history) / len(ram_history), 2)
+        payload[node]["cpu_score"] = cpu_score
+        payload[node]["ram_score"] = ram_score
+
+    requests.post(simulator_url + '/update_scores', json=payload)
