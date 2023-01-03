d59d01e43c6fe2dc72f36b1b141598a2c0850144
Nerea PRUNEAU MARIN <nerea-externe.pruneau-marin@enedis.fr>
2023-01-03 17:19:39 +0100

    ajout voiture

--- front/src/app/field/field.component.ts
+++ front/src/app/field/field.component.ts
@@ -13,13 +13,19 @@ export class FieldComponent implements OnInit {
 
   private jaune: string = '#FFD733';
   private vert: string = '#94D800';
+  private voiture: string = '#5E5C1A';
+  private silver: string = '#B2B2B2';
+
+  private x_logo!: number;
+  private y_logo!: number;
+  private size_logo: number = 7.5;
 
   ngOnInit(): void {
     const canvas: HTMLCanvasElement = this.field.nativeElement;
     const context = canvas.getContext('2d');
     if (context) {
       this.#drawCircle(context, this.jaune, 500, 500);
-      this.#drawCar(context);
+      this.#drawCar(context, 100, 100);
     }
   }
 
@@ -31,32 +37,77 @@ export class FieldComponent implements OnInit {
   ) {
     context.fillStyle = color;
     context.beginPath();
-    context.arc(x, y, 20, (Math.PI / 180) * 0, (Math.PI / 180) * 360);
+    context.arc(x, y, 15, (Math.PI / 180) * 0, (Math.PI / 180) * 360);
     context.fill();
   }
 
-  #drawCar(context: CanvasRenderingContext2D) {
+  #drawCar(context: CanvasRenderingContext2D, x: number, y: number) {
     // chassis
-    context.fillStyle = 'black';
-    context.fillRect(40, 65, 110, 25);
-
-    // cabine x : 50, y : 65
-    context.beginPath();
-    context.moveTo(100, 65);
-    context.lineTo(50 + 20, 65);
-    context.lineTo(50 + 20 + 15 / 2, 65 + 20);
-    context.lineTo(50 + 20 / 2, 65 + 20);
-    context.fill();
+    context.fillStyle = this.voiture;
+    context.fillRect(x - 37.5, y - 10, 75, 15);
 
     // back wheel
     context.fillStyle = 'black';
     context.beginPath();
-    context.arc(65, 85, 15, 0, 2 * Math.PI);
+    context.arc(x - 17.5, y + 7, 7.5, 0, 2 * Math.PI);
+    context.fill();
+    context.fillStyle = this.silver;
+    context.beginPath();
+    context.arc(x - 17.5, y + 7, 3, 0, 2 * Math.PI);
     context.fill();
 
     // front wheel
+    context.fillStyle = 'black';
     context.beginPath();
-    context.arc(125, 85, 15, 0, 2 * Math.PI);
+    context.arc(x + 17.5, y + 7, 7.5, 0, 2 * Math.PI);
+    context.fill();
+    context.fillStyle = this.silver;
+    context.beginPath();
+    context.arc(x + 17.5, y + 7, 3, 0, 2 * Math.PI);
+    context.fill();
+
+    // cabin
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.moveTo(x - 12.5, y - 17.5);
+    context.lineTo(x + 12.5, y - 17.5);
+    context.lineTo(x + 22.5, y - 9);
+    context.lineTo(x - 22.5, y - 9);
+    context.fill();
+
+    // logo
+    this.x_logo = x + 32.5;
+    this.y_logo = y - 16;
+    context.strokeStyle = this.silver;
+    context.fillStyle = this.silver;
+    context.fillRect(
+      this.x_logo - 0.5,
+      this.y_logo + this.size_logo / 2 + 1,
+      1,
+      1
+    );
+    context.beginPath();
+    context.arc(this.x_logo, this.y_logo, this.size_logo / 2, 0, 2 * Math.PI);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(this.x_logo, this.y_logo - this.size_logo / 2);
+    context.lineTo(
+      this.x_logo - (Math.sqrt(3) / 2) * (this.size_logo * 0.05),
+      this.y_logo - 0.5 * (this.size_logo * 0.05)
+    );
+    context.lineTo(
+      this.x_logo - (Math.sqrt(3) / 2) * (this.size_logo / 2),
+      this.y_logo + 0.5 * (this.size_logo / 2)
+    );
+    context.lineTo(this.x_logo, this.y_logo + this.size_logo * 0.05);
+    context.lineTo(
+      this.x_logo + (Math.sqrt(3) / 2) * (this.size_logo / 2),
+      this.y_logo + 0.5 * (this.size_logo / 2)
+    );
+    context.lineTo(
+      this.x_logo + (Math.sqrt(3) / 2) * (this.size_logo * 0.05),
+      this.y_logo - 0.5 * (this.size_logo * 0.05)
+    );
     context.fill();
   }
 }
