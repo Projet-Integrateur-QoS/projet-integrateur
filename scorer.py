50193e9882fcf4a76b1f5a77da36eff0ea3942c8
Djihadi Ahamdy <djihadi.ahamdy@gmail.com>
2023-01-03 17:17:38 +0100

    Add TrustMan(Trueman custom) for scoring

--- scorer.py
+++ scorer.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import time
+import trustman
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,13 +15,18 @@ while True:
     payload = {}
 
     nodes = requests.get(simulator_url).json()
-    for node in nodes:
-        payload[node] = {}
-        cpu_history = [float(cpu) for cpu in nodes[node]["cpu"]]
-        ram_history = [float(ram) for ram in nodes[node]["ram"]]
-        cpu_score = round(sum(cpu_history) / len(cpu_history), 2)
-        ram_score = round(sum(ram_history) / len(ram_history), 2)
-        payload[node]["cpu_score"] = cpu_score
-        payload[node]["ram_score"] = ram_score
+
+    payload = trustman.Trustman_Scorer(nodes)
+
+    # for node in nodes:
+    #     payload[node] = {}
+    #     cpu_history = [float(cpu) for cpu in nodes[node]["cpu"]]
+    #     ram_history = [float(ram) for ram in nodes[node]["ram"]]
+    #     cpu_score = round(sum(cpu_history) / len(cpu_history), 2)
+    #     ram_score = round(sum(ram_history) / len(ram_history), 2)
+    #     payload[node]["cpu_score"] = cpu_score
+    #     payload[node]["ram_score"] = ram_score
+
+
 
     requests.post(simulator_url + '/update_scores', json=payload)
