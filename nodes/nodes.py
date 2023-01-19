ec7abf644dd73f693511d6fce9032ebb03662525
pgervais17 <p.gervais17@laposte.net>
2023-01-19 17:03:26 +0100

    Correction node

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -18,39 +18,35 @@ with open('input.csv') as csvfile:
         time.sleep(int(os.environ['NODES_INTERVAL']))
 
         lat_a, lat_b, lat_c, cpu_a, ram_a, cpu_b, ram_b, cpu_c, ram_c, x_c, y_c, x_car, y_car = row
-        payload = [
-            {
-                "id": 0,
-                "lat": lat_a,
-                "cpu": cpu_a,
-                "ram": ram_a,
-                "x": "0",
-                "y": "0",
-            },
-            {
-                "id": 1,
-                "lat": lat_b,
-                "cpu": cpu_b,
-                "ram": ram_b,
-                "x": "1",
-                "y": "2",
-            },
-            {
-                "id": 2,
-                "lat": lat_c,
-                "cpu": cpu_c,
-                "ram": ram_c,
-                "x": x_c,
-                "y": y_c,
-            },
-            {
-                "id": 3,
-                "lat": 0,
-                "cpu": 0,
-                "ram": 0,
-                "x": x_car,
-                "y": y_car,
-            }
-        ]
+        payload = {
+            "nodes": [
+                {
+                    "id": 0,
+                    "lat": lat_a,
+                    "cpu": cpu_a,
+                    "ram": ram_a,
+                    "x": "0",
+                    "y": "0",
+                },
+                {
+                    "id": 1,
+                    "lat": lat_b,
+                    "cpu": cpu_b,
+                    "ram": ram_b,
+                    "x": "1",
+                    "y": "2",
+                },
+                {
+                    "id": 2,
+                    "lat": lat_c,
+                    "cpu": cpu_c,
+                    "ram": ram_c,
+                    "x": x_c,
+                    "y": y_c,
+                },
+            ],
+            "car_x": x_car,
+            "car_y": y_car,
+        }
         requests.post(simulator_url + '/new', json=payload)
         print(f"sent row {payload}")
